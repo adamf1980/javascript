9d926e553845b010a45464552d82938e4a7196b0
Adam <worksmarter.adam@gmail.com>
2017-08-31 20:38:54 -0500

    Javascript assignments

--- lib.js
+++ lib.js
@@ -26,6 +26,29 @@ const calculateTotals =
   listings =>
     carts => {
       // TODO
+      let resultArray = []
+      let price = 0
+      for (let x = 0; x < carts.length;x++){
+        let cartTotal = 0
+        let cart = carts[x]
+        let cust= cart.customer
+        let items = cart.items
+        for (let y = 0; y < items.length; y++){
+          let item = items[y]
+          for (let z = 0; z < listings.length; z++){
+            let listing = listings[z]
+            if (listing.name === item)
+              price = listedPrice(listing)(item)
+          } //listings loop
+          cartTotal += price
+        } //items loop
+        let newOb = {
+          customer: cust,
+          total: cartTotal
+        } //ob 
+        resultArray.push(newOb)
+      }
+      return resultArray
     }
 
 module.exports = {
